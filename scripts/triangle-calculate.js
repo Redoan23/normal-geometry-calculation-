c1cfa0060689ea4eb40811038126acc501141ca2
Redoan23 <redoan1080@gmail.com>
2024-02-13 09:47:05 +0600

    first commit

---
+++ scripts/triangle-calculate.js
@@ -0,0 +1,13 @@
+function calculateTriangleArea(){
+  let base=document.getElementById('triangle-base')
+  let height=document.getElementById('triangle-height')
+  let baseValue=parseFloat(base.value);
+  let heightValue=parseFloat(height.value);
+  let total=0.5*baseValue*heightValue;
+//   console.log(total);
+
+  let p=document.createElement('p');
+  p.innerText=total
+
+  document.getElementById('result').appendChild(p);
+}
